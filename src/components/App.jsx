92bc0a6fc32c01ada3f807909de010b7a991e1e3
SachinT-K <75876472+SachinT-K@users.noreply.github.com>
2021-06-05 16:23:10 +0530

    React Hook - useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  setInterval(updateTime, 1000);
+  
+  let time = new Date().toLocaleTimeString();
+
+  const [newTime, setTime] = useState(time);
+
+  function updateTime() {
+    const now = new Date().toLocaleTimeString();
+    setTime(now);
+  }
+
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{newTime}</h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
   );
 }
